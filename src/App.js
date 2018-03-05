34e920a35077358de2e077ff71fc18bda2314e18
Dale Pepper <dale_pepper@hotmail.com>
2018-03-05 15:07:05 +1100

    Added API call. Added basic styling and markup for response.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,76 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      //result: {},
+      income: [],
+      outgoing: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8888")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            income: result.income,
+            outgoing: result.outgoing
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
+
+    const { error, isLoaded, income, outgoing} = this.state;
+
+    if(error) {
+      return <div>Error: {error.message}</div>;
+    }
+    else if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    else {
+      return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Budgeting App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="left">
+          <h3>Income</h3>
+          {income.map(item => (
+            <p>
+              <span>{item.name}</span>
+              <span>{item.amount}</span>
+            </p>
+          ))}
+          <button>Add new item</button>
+        </div>
+        <div className="left">
+          <h3>Outgoing</h3>
+          {outgoing.map(item => (
+            <p>
+              <span>{item.name}</span>
+              <span>{item.amount}</span>
+            </p>
+          ))}
+          <button>Add new item</button>
+        </div>
       </div>
-    );
+    )};
   }
 }
 
